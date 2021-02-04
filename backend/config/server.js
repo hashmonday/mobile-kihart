ae0beb83f0823c7603d5d8482480f415a0dc05ea
Suwicha Khaosamlee <hashmonday@gmail.com>
2021-02-04 18:00:04 +0700

    Setup Project
    - Frontend Nuxt + Tailwind + Dayjs
    - Backend Strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c3d41e279724e6cb33e168ddf635acc3'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '42943129-ef96-4280-9d0c-2f3b147a06f4'
+};
