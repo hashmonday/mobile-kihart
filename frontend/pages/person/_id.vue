593a1799bf53f3f0a27e01d4abbad3f22992c65a
Suwicha Khaosamlee <hashmonday@gmail.com>
2021-02-10 18:01:09 +0700

    fixed edit path

--- frontend/pages/person/_id.vue
+++ frontend/pages/person/_id.vue
@@ -9,7 +9,7 @@
           กลับหน้าหลัก
         </button>
       </NuxtLink>
-      <NuxtLink :to="`/person/${$route.params.id}/edit`">
+      <NuxtLink :to="'/person/edit/' + $route.params.id">
         <button
           type="button"
           class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
